fce5065348c4dc1a9f6f1fb47b4ec611a763a229
Mikhail Kolesnik <mike@openbunker.org>
2012-01-17 11:28:40 +0200

    Drop COMPASS_QUIET option, provide more flesible COMPASS_EXTRA_OPTS, misc cleanups.

--- orienteer/templatetags/orienteer.py
+++ orienteer/templatetags/orienteer.py
@@ -21,59 +21,71 @@ __version__ = '0.2'
 import os
 import re
 import sys
-from commands import getstatusoutput
+import subprocess
 
 from django import template
 from django.conf import settings
 
 
+def run(cmd):
+    """Return (status, output) of executing cmd."""
+    pipe = subprocess.Popen(cmd, shell=False, universal_newlines=True,
+                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output = str.join('', pipe.stdout.readlines())
+    sts = pipe.wait()
+    if sts is None:
+        sts = 0
+    return sts, output
+
 register = template.Library()
 
 @register.simple_tag
 def compass(filename, media):
-    proj_dir = settings.COMPASS_PROJECT_DIR
-    source_dir = getattr(settings, 'COMPASS_SOURCE_DIR', 'src')
+    binary = settings.COMPASS_BIN
     output_dir = settings.COMPASS_OUTPUT_DIR
     output_url = settings.COMPASS_OUTPUT_URL
-    quiet_mode = getattr(settings, 'COMPASS_QUIET', False)
+    project_dir = settings.COMPASS_PROJECT_DIR
+    source_dir = getattr(settings, 'COMPASS_SOURCE_DIR', 'src')
+    extra_opts = re.split("\s", getattr(settings, 'COMPASS_EXTRA_OPTS', ''))
     use_timestamp = getattr(settings, 'COMPASS_USE_TIMESTAMP', True)
-    
+    debug = getattr(settings, 'COMPASS_DEBUG', settings.TEMPLATE_DEBUG)
+
     # get timestamp of css, if it doesn't exist we need to make it
     try:
-        output_file_stat = os.stat(os.path.join(proj_dir, output_dir, filename + '.css'))
+        output_file_stat = os.stat(os.path.join(project_dir, output_dir, filename + '.css'))
         output_file_ts = output_file_stat.st_mtime
     except OSError:
         output_file_ts = 1
-    
+
     source_file_ts = 0
-    
-    for root, dirs, files in os.walk(os.path.join(proj_dir, source_dir)):
+
+    for root, dirs, files in os.walk(os.path.join(project_dir, source_dir)):
         for source_file in files:
             if source_file[-5:].lower() == '.scss':
                 source_file_stat = os.stat(os.path.join(root, source_file))
                 if source_file_stat.st_mtime > source_file_ts:
                     source_file_ts = source_file_stat.st_mtime
-    
+
     needs_update = source_file_ts > output_file_ts
-    
-    css = "<link rel='stylesheet' href='%s%s' type='text/css' media='%s' />" % (output_url + filename + '.css', use_timestamp and '?%s' % output_file_ts or '', media)
-    
-    # if we aren't debugging (in production for example), short-cicuit this madness
+
+    css = "<link rel='stylesheet' href='%s%s' type='text/css' media='%s'/>" % \
+          (output_url + filename + '.css',
+           use_timestamp and '?%s' % output_file_ts or '',
+           media)
+
     if not settings.TEMPLATE_DEBUG or not needs_update:
         return css
-    
-    cmd_dict = {
-        'bin': settings.COMPASS_BIN, 
-        'sass_style': settings.COMPASS_STYLE, 
-        'project_dir': proj_dir,  
-        'output_dir': output_dir, 
-        'quiet_flag': quiet_mode and "--quiet" or ""
-    }
-    
-    cmd = "%(bin)s compile %(quiet_flag)s -s %(sass_style)s --css-dir %(output_dir)s %(project_dir)s" % cmd_dict
-    (status, output) = getstatusoutput(cmd)
+
+    cmd = [binary, 'compile', '--css-dir', output_dir,
+           '--sass-dir', source_dir, project_dir]
+    # avoid passing empty options in cmd array
+    if extra_opts:
+        cmd.extend(extra_opts)
+    if debug:
+        sys.stderr.write(' '.join(cmd))
+    (status, output) = run(cmd)
     if output:
-        for line in re.split("\n+", output):
+        for line in re.split("\n+", output.strip()):
             sys.stderr.write(re.sub(r'^', '[Compass] ', line) + "\n")
-    
+
     return css
