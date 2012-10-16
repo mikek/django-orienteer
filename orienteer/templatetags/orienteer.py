5b1b4de74957f323534a61d7e2bcaaf4800fc510
Mikhail Kolesnik <mike@openbunker.org>
2012-10-16 13:44:52 +0300

    some pep8 fixes

--- orienteer/templatetags/orienteer.py
+++ orienteer/templatetags/orienteer.py
@@ -31,8 +31,9 @@ from django.conf import settings
 
 def run(cmd, cwd=None):
     """Return (status, output) of executing cmd."""
-    pipe = subprocess.Popen(cmd, shell=False, executable=cmd[0], cwd=cwd, universal_newlines=True,
-                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    pipe = subprocess.Popen(cmd, shell=False, executable=cmd[0], cwd=cwd,
+                            universal_newlines=True, stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
     output = str.join('', pipe.stdout.readlines())
     sts = pipe.wait()
     if sts is None:
@@ -40,6 +41,7 @@ def run(cmd, cwd=None):
     return sts, output
 
 register = template.Library()
+
 
 @register.simple_tag
 def compass(filename, media):
@@ -49,7 +51,9 @@ def compass(filename, media):
     output_dir = getattr(settings, 'COMPASS_OUTPUT_DIR', './')
     source_dir = getattr(settings, 'COMPASS_SOURCE_DIR', 'src')
     use_timestamp = getattr(settings, 'COMPASS_USE_TIMESTAMP', True)
-    skip_on_equal_timestamp = getattr(settings, 'COMPASS_SKIP_ON_EQUAL_TIMESTAMP', False)
+    skip_on_equal_timestamp = getattr(settings,
+                                      'COMPASS_SKIP_ON_EQUAL_TIMESTAMP',
+                                      False)
     debug = getattr(settings, 'COMPASS_DEBUG', settings.DEBUG)
 
     output_url = getattr(settings, 'COMPASS_OUTPUT_URL', None)
@@ -64,7 +68,8 @@ def compass(filename, media):
 
     # get timestamp of css, if it doesn't exist we need to make it
     try:
-        output_file_stat = os.stat(os.path.join(project_dir, output_dir, filename + '.css'))
+        output_file_stat = os.stat(os.path.join(project_dir, output_dir,
+                                                filename + '.css'))
         output_file_ts = output_file_stat.st_mtime
     except OSError:
         output_file_ts = 0
@@ -91,7 +96,7 @@ def compass(filename, media):
                 if source_file_stat.st_mtime > source_file_ts:
                     source_file_ts = source_file_stat.st_mtime
 
-    if ((source_file_ts < output_file_ts) or 
+    if ((source_file_ts < output_file_ts) or
             (skip_on_equal_timestamp and source_file_ts == output_file_ts)):
         return css
 
